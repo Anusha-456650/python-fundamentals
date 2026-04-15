43cd61786c7f02d0fac5f859b80bf2f469bd43d7
Anusha-456650 <mandadam.456@gmail.com>
2026-04-15 14:34:06 -0400

    add first python file and variables practice covering datatypes

---
+++ basics.py
@@ -0,0 +1,2 @@
+# First python file pushed to GitHub to practice git workflow
+print("My first git project in python")
---
+++ variables.py
@@ -0,0 +1,10 @@
+# Demonstrates the use of variables and basic data types in python
+
+name = "Misha" # String: stores the user's name
+age = 20       # Integer: Stores the user's age
+height = 5.4   # Float: Stores the user's height in feet
+
+# Displaying stored values in a structured format
+print("Name:", name)
+print("Age:",age)
+print("Height:",height)
