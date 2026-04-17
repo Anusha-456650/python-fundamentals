02f29da6292823bf5f8b3ab079d23b78f5d202b1
Anusha-456650 <mandadam.456@gmail.com>
2026-04-17 09:14:38 -0400

    update while loop example to use dynamic user input istead of hardcoded values

---
+++ while_loop_basics.py
@@ -0,0 +1,9 @@
+# Demonstrates while loop
+
+# Taking input from the user
+limit = int(input("Please enter a limit: "))
+count = 1
+
+while count <= limit:
+    print(f'count : {count}')
+    count += 1
